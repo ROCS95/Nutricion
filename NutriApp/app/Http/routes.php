a3ae59fbbe9ce300bec1df50aec9c5e5adabc84a
juan jose <chepe0794@hotmail.com>
2016-08-09 11:13:44 -0600

    ya bretea el crud paciente

--- NutriApp/app/Http/routes.php
+++ NutriApp/app/Http/routes.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 
 Route::auth();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/home', 'PacienteController@index');
 
 //Route::controller("cliente","ClienteController");
 
--- NutriApp/resources/views/welcome.blade.php
+++ NutriApp/resources/views/welcome.blade.php
@@ -1,49 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-                <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Paciente
-                        <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Nuevo</a></li>
-                        <li><a href="#">Ver todos</a></li>
-                    </ul>
+                    <div class="panel-body">
+                        You are logged in!
+                    </div>
                 </div>
-            </div>
-                <div>
-                    <table class="table table-condensed">
-
-
-                        <!-- On cells (`td` or `th`) -->
-                        <tr>
-                            <td class="active">Nombre</td>
-                            <td class="success">Apellido</td>
-                            <td class="warning">Cedula</td>
-                            <td class="danger">Otra</td>
-                            <td class="info">Algo más</td>
-                            <td class="active">Algo más 2</td>
-                        </tr>
-                        <tr>
-                            <td class="active">Nombre</td>
-                            <td class="success">Apellido</td>
-                            <td class="warning">Cedula</td>
-                            <td class="danger">Otra</td>
-                            <td class="info">Algo más</td>
-                            <td class="active">Algo más 2</td>
-                        </tr>
-                    </table>
-                </div>
-
             </div>
         </div>
     </div>
-</div>
 @endsection
