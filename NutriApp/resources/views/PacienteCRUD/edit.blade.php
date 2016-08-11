1e66122ad5e078f69744c7a1d41e7392bf4e84a3
Rocs95 <chacon9595@hotmail.com>
2016-08-11 13:03:51 -0500

    gg

--- NutriApp/resources/views/PacienteCRUD/edit.blade.php
+++ NutriApp/resources/views/PacienteCRUD/edit.blade.php
@@ -1,69 +1,149 @@
-@extends('layouts.default')
+@extends('layouts.app')
 @section('content')
 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Edit New Paciente</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('paciente.index') }}"> Back</a>
-            </div>
-        </div>
-    </div>
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    {!! Form::model($item, ['method' => 'PATCH','route' => ['paciente.update', $item->id]]) !!}
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Nombre:</strong>
-                {!! Form::text('nombre', null, array('placeholder' => 'Nombre','class' => 'form-control')) !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Apellido:</strong>
-                {!! Form::text('apellido', null, array('placeholder' => 'Apellido','class' => 'form-control')) !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Cédula:</strong>
-                {!! Form::text('cedula', null, array('placeholder' => 'Cédula','class' => 'form-control')) !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Teléfono:</strong>
-                {!! Form::text('telefono', null, array('placeholder' => 'Teléfono','class' => 'form-control')) !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Dirección:</strong>
-                {!! Form::text('direccion', null, array('placeholder' => 'Dirección','class' => 'form-control')) !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Email:</strong>
-                {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
-            </div>
-        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-lg-12 margin-tb">
+                                <div class="text-center">
+                                    <h2>Modificar Datos Paciente</h2>
+                                </div>
+                                <br>
+                                <br>
+                            </div>
+                        </div>
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        {!! Form::model($paciente, ['method' => 'PATCH','route' => ['pacienteCRUD.update', $paciente->id]]) !!}
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Nombre:</strong>
+                                    {!! Form::text('nombre', null, array('placeholder' => 'Nombre','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Apellido:</strong>
+                                    {!! Form::text('apellido', null, array('placeholder' => 'Apellido','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Cédula:</strong>
+                                    {!! Form::text('cedula', null, array('placeholder' => 'Cédula','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Teléfono:</strong>
+                                    {!! Form::text('telefono', null, array('placeholder' => 'Teléfono','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Dirección:</strong>
+                                    {!! Form::text('direccion', null, array('placeholder' => 'Dirección','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Email:</strong>
+                                    {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
+                                </div>
+                            </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary">edit</button>
+                            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-lg-12 margin-tb">
+                                <div class="text-center">
+                                    <h2>Modificar Pesos Paciente</h2>
+                                </div>
+                                <div class="pull-right">
+                                    <a class="btn btn-primary" href="{{ route('pacienteCRUD.index') }}"> Back</a>
+                                </div>
+                            </div>
+                        </div>
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        {!! Form::model($paciente, ['method' => 'PATCH','route' => ['pacienteCRUD.update', $paciente->id]]) !!}
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Masa Corporal:</strong>
+                                    {!! Form::text('masa_corporal', null, array('placeholder' => 'Masa Corporal','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Peso:</strong>
+                                    {!! Form::text('peso', null, array('placeholder' => 'Peso','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Altura:</strong>
+                                    {!! Form::text('altura', null, array('placeholder' => 'Altura','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Edad:</strong>
+                                    {!! Form::text('edad', null, array('placeholder' => 'Edad','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Masa Muscular:</strong>
+                                    {!! Form::text('masa_muscular', null, array('placeholder' => 'Masa Muscular','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Masa Osea:</strong>
+                                    {!! Form::text('masa_osea', null, array('placeholder' => 'Masa Osea','class' => 'form-control')) !!}
+                                </div>
+                            </div>
+
+                            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                <button type="submit" class="btn btn-primary">edit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+
+
     {!! Form::close() !!}
 
 @endsection
--- NutriApp/resources/views/PacienteCRUD/index.blade.php
+++ NutriApp/resources/views/PacienteCRUD/index.blade.php
@@ -1,57 +1,74 @@
-@extends('layouts.default')
+@extends('layouts.app')
 @section('content')
 
     <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Pacientes CRUD</h2>
+            <div class="text-center">
+                <h2>Datos de pacientes</h2>
             </div>
-            <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('paciente.create') }}"> Create New Paciente</a>
+
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <div class="dropdown">
+                            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                Mantenimiento
+                                <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                                <li><a href="{{ route('pacienteCRUD.create') }}">Nuevo paciente</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="panel-body">
+                        @if ($message = Session::get('success'))
+                            <div class="alert alert-success">
+                                <p>{{ $message }}</p>
+                            </div>
+
+                        @endif
+
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>No</th>
+                                <th>Nombre</th>
+                                <th>Apellidos</th>
+                                <th>Cedula</th>
+                                <th>Telefono</th>
+                                <th>Direccion</th>
+                                <th>Email</th>
+                                <th width="280px">Action</th>
+                            </tr>
+
+                            @foreach ($pacientes as $key => $paciente)
+                                <tr>
+                                    <td>{{ ++$i }}</td>
+                                    <td>{{ $paciente->nombre }}</td>
+                                    <td>{{ $paciente->apellido }}</td>
+                                    <td>{{ $paciente->cedula }}</td>
+                                    <td>{{ $paciente->telefono }}</td>
+                                    <td>{{ $paciente->direccion }}</td>
+                                    <td>{{ $paciente->email }}</td>
+                                    <td>
+                                        <a class="btn btn-info" href="{{ route('pacienteCRUD.show',$paciente->id) }}">Show</a>
+                                        <a class="btn btn-primary" href="{{ route('pacienteCRUD.edit',$paciente->id) }}">Edit</a>
+                                        {!! Form::open(['method' => 'DELETE','route' => ['pacienteCRUD.destroy', $paciente->id],'style'=>'display:inline']) !!}
+                                        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                                        {!! Form::close() !!}
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </table>
+
+                        {!! $pacientes->render() !!}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-
-    @endif
-
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Nombre</th>
-            <th>Apellidos</th>
-            <th>Cedula</th>
-            <th>Telefono</th>
-            <th>Direccion</th>
-            <th>Email</th>
-            <th width="280px">Action</th>
-        </tr>
-
-        @foreach ($items as $key => $item)
-            <tr>
-                <td>{{ ++$i }}</td>
-                <td>{{ $item->nombre }}</td>
-                <td>{{ $item->apellido }}</td>
-                <td>{{ $item->cedula }}</td>
-                <td>{{ $item->telefono }}</td>
-                <td>{{ $item->direccion }}</td>
-                <td>{{ $item->email }}</td>
-                <td>
-                    <a class="btn btn-info" href="{{ route('paciente.show',$item->id) }}">Show</a>
-                    <a class="btn btn-primary" href="{{ route('paciente.edit',$item->id) }}">Edit</a>
-                    {!! Form::open(['method' => 'DELETE','route' => ['paciente.destroy', $item->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                    {!! Form::close() !!}
-                </td>
-            </tr>
-        @endforeach
-
-    </table>
-
-    {!! $items->render() !!}
 
 @endsection
